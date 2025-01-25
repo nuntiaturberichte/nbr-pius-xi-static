938145f3f2cf7cc5defdd9ae5a56fd60d48d0d47
kroissenbrunner.martin <kroissenbrunner.martin@gmail.com>
2025-01-25 10:57:25 +0100

    copied and adapted from gn

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,14 +1,25 @@
-# bin/bash
+#!/bin/bash
 
-echo "fetching transkriptions from data_repo"
-rm -rf data/
-curl -LO https://github.com/nuntiaturberichte/nbr-pius-xi-data/archive/refs/heads/main.zip
-unzip main
+echo "Fetching transcriptions from data_repo"
 
-mv ./nbr-pius-xi-data-main/data/ .
+# Lösche und erstelle neue Verzeichnisse für die Daten
+rm -rf data/ && mkdir data
 
+# Lade das Repository als ZIP-Archiv herunter
+curl -LO https://codeload.github.com/nuntiaturberichte/nbr-pius-xi-data/zip/refs/heads/default
+
+# Entpacke das heruntergeladene ZIP-Archiv
+unzip main.zip
+
+# # Verschiebe die benötigten Ordner aus dem tieferen Verzeichnis in die entsprechenden Verzeichnisse unter "data"
+mv ./nbr-pius-xi-data-main/* ./data
+
+
+# # Bereinige: Lösche das heruntergeladene ZIP-Archiv und das entpackte Verzeichnis
 rm main.zip
 rm -rf ./nbr-pius-xi-data-main
 
-echo "fetch imprint"
+echo "Transcriptions have been successfully updated."
+
+echo "fetching imprint"
 ./shellscripts/dl_imprint.sh
