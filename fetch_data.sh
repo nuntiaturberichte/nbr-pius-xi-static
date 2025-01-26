915507828c3cc26caabf3b578df157d1b1d02b08
kroissenbrunner.martin <kroissenbrunner.martin@gmail.com>
2025-01-26 13:17:22 +0100

    debugging

--- fetch_data.sh
+++ fetch_data.sh
@@ -6,18 +6,23 @@ echo "Fetching transcriptions from data_repo"
 rm -rf data/ && mkdir data
 
 # Lade das Repository als ZIP-Archiv herunter
+echo "Downloading repo as zip-file"
 curl -LO https://codeload.github.com/nuntiaturberichte/nbr-pius-xi-data/zip/refs/heads/default
 ls -l
 
 # Entpacke das heruntergeladene ZIP-Archiv
-unzip default.zip
+echo "unzipping zip-file"
+unzip default
+ls -l
 
 # # Verschiebe die benötigten Ordner aus dem tieferen Verzeichnis in die entsprechenden Verzeichnisse unter "data"
+echo "moving content from unpacked zip folder to ./data"
 mv ./nbr-pius-xi-data-default/* ./data
 
 
 # # Bereinige: Lösche das heruntergeladene ZIP-Archiv und das entpackte Verzeichnis
-rm nbr-pius-xi-data-default.zip
+echo "removing zip-file and its unpacked content"
+rm default
 rm -rf ./nbr-pius-xi-data-default
 
 echo "Transcriptions have been successfully updated."
