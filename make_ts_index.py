3f44b2386691ac5744753ecf71b1386690a208b5
kroissenbrunner.martin <kroissenbrunner.martin@gmail.com>
2025-02-28 08:11:22 +0100

    first draft

--- make_ts_index.py
+++ make_ts_index.py
@@ -1,148 +1,153 @@
+# mit github action - Anfang
+"""
 import glob
 import os
 
 from typesense.api_call import ObjectNotFound
-from acdh_cfts_pyutils import TYPESENSE_CLIENT as client
+from acdh_cfts_pyutils import TYPESENSE_CLIENT as client, CFTS_COLLECTION
+from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import extract_fulltext, normalize_string
+from tqdm import tqdm
+"""
+# mit github action - Ende
+
+# für lokale Ausführung - Anfang
+import glob
+import os
+import typesense
+
+from typesense.api_call import ObjectNotFound
 from acdh_cfts_pyutils import CFTS_COLLECTION
 from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import extract_fulltext, normalize_string
 from tqdm import tqdm
 
+TYPESENSE_API_KEY = "xyz"
+TYPESENSE_HOST = "localhost"
+TYPESENSE_PORT = "8108"
 
-files = glob.glob("./data/editions/*/*.xml")
+client = typesense.Client({
+    "nodes": [{
+        "host": TYPESENSE_HOST,
+        "port": TYPESENSE_PORT,
+        "protocol": "http",
+    }],
+    "api_key": TYPESENSE_API_KEY,
+    "connection_timeout_seconds": 10
+})
+# für lokale Ausführung - Ende
 
+files = glob.glob("./data/editions/**/*.xml", recursive=True)
+tag_blacklist = ["{http://www.tei-c.org/ns/1.0}abbr"]
 
+COLLECTION_NAME = "nbr-pius-xi"
+
+# Falls die Collection existiert, löschen und neu erstellen
 try:
-    client.collections["Nuntiatur Pius XI"].delete()
+    client.collections[COLLECTION_NAME].delete()
 except ObjectNotFound:
     pass
 
+# Collection-Schema definieren
 current_schema = {
-    "name": "Nuntiatur Pius XI",
+    "name": COLLECTION_NAME,
+    "enable_nested_fields": True,
     "fields": [
-        {"name": "id", "type": "string"},
-        {"name": "rec_id", "type": "string"},
-        {"name": "title", "type": "string"},
-        {"name": "full_text", "type": "string"},
+        {"name": "id", "type": "string", "sort": True},
+        {"name": "rec_id", "type": "string", "sort": True},
+        {"name": "title", "type": "string", "sort": True},
+        {"name": "full_text", "type": "string", "sort": True},
         {
             "name": "year",
             "type": "int32",
             "optional": True,
             "facet": True,
+            "sort": True,
         },
-        {"name": "persons", "type": "string[]", "facet": True, "optional": True},
-        {"name": "places", "type": "string[]", "facet": True, "optional": True},
-        {"name": "orgs", "type": "string[]", "facet": True, "optional": True},
+        {
+            "name": "sender",
+            "type": "string[]",
+            "optional": True,
+            "facet": True,
+            "sort": False,
+        },
+        {
+            "name": "receiver",
+            "type": "string[]",
+            "optional": True,
+            "facet": True,
+            "sort": False,
+        },
     ],
 }
 
 client.collections.create(current_schema)
 
-
-def get_entities(ent_type, ent_node, ent_name):
-    entities = []
-    e_path = f'.//tei:rs[@type="{ent_type}"]/@ref'
-    for p in body:
-        ent = p.xpath(e_path, namespaces={"tei": "http://www.tei-c.org/ns/1.0"})
-        ref = [ref.replace("#", "") for e in ent if len(ent) > 0 for ref in e.split()]
-        for r in ref:
-            p_path = f'.//tei:{ent_node}[@xml:id="{r}"]//tei:{ent_name}[1]'
-            en = doc.any_xpath(p_path)
-            if en:
-                entity = " ".join(" ".join(en[0].xpath(".//text()")).split())
-                if len(entity) != 0:
-                    entities.append(entity)
-                else:
-                    with open("log-entities.txt", "a") as f:
-                        f.write(f"{r} in {record['id']}\n")
-    return [ent for ent in sorted(set(entities))]
-
-
+# XML-Dateien in die Collection einfügen
 records = []
 cfts_records = []
 for x in tqdm(files, total=len(files)):
-    doc = TeiReader(xml=x, xsl="./xslt/preprocess_typesense.xsl")
-    facs = doc.any_xpath(".//tei:body/tei:div/tei:pb/@facs")
-    pages = 0
-    for v in facs:
-        p_group = f".//tei:body/tei:div/tei:p[preceding-sibling::tei:pb[1]/@facs='{v}']|.//tei:body/tei:div/tei:lg[preceding-sibling::tei:pb[1]/@facs='{v}']"
-        body = doc.any_xpath(p_group)
-        pages += 1
-        cfts_record = {
-            "project": "Nuntiatur Pius XI",
-        }
-        record = {}
-        record["id"] = os.path.split(x)[-1].replace(".xml", f".html?tab={str(pages)}")
-        cfts_record["id"] = record["id"]
-        cfts_record["resolver"] = f"https://github.com/nuntiaturberichte/nbr-pius-xi-static/{record['id']}"
-        record["rec_id"] = os.path.split(x)[-1]
-        cfts_record["rec_id"] = record["rec_id"]
-        r_title = " ".join(
-            " ".join(
-                doc.any_xpath('.//tei:titleStmt/tei:title[@level="a"]/text()')
-            ).split()
-        )
-        record["title"] = f"{r_title} Page {str(pages)}"
-        cfts_record["title"] = record["title"]
-        try:
-            date_str = doc.any_xpath("//tei:origin/tei:origDate/@notBefore")[0]
-        except IndexError:
-            date_str = doc.any_xpath("//tei:origin/tei:origDate/text()")[0]
-            data_str = date_str.split("--")[0]
-            if len(date_str) > 3:
-                date_str = date_str
-            else:
-                date_str = "1959"
+    cfts_record = {
+        "project": COLLECTION_NAME,
+    }
+    record = {}
 
-        try:
-            record["year"] = int(date_str[:4])
-            cfts_record["year"] = int(date_str[:4])
-        except ValueError:
-            pass
+    doc = TeiReader(x)
+    try:
+        body = doc.any_xpath(".//tei:body")[0]
+    except IndexError:
+        continue
 
-        if len(body) > 0:
-            # get unique persons per page
-            ent_type = "person"
-            ent_name = "persName"
-            record["persons"] = get_entities(
-                ent_type=ent_type, ent_node=ent_type, ent_name=ent_name
-            )
-            cfts_record["persons"] = record["persons"]
-            # get unique places per page
-            ent_type = "place"
-            ent_name = "placeName"
-            record["places"] = get_entities(
-                ent_type=ent_type, ent_node=ent_type, ent_name=ent_name
-            )
-            cfts_record["places"] = record["places"]
-            # get unique orgs per page
-            ent_type = "org"
-            ent_name = "orgName"
-            record["orgs"] = get_entities(
-                ent_type=ent_type, ent_node=ent_type, ent_name=ent_name
-            )
-            cfts_record["orgs"] = record["orgs"]
-            # get unique bibls per page
-            ent_type = "lit_work"
-            ent_name = "title"
-            ent_node = "bibl"
-            record["works"] = get_entities(
-                ent_type=ent_type, ent_node=ent_node, ent_name=ent_name
-            )
-            cfts_record["works"] = record["works"]
-            record["full_text"] = "\n".join(
-                " ".join("".join(p.itertext()).split()) for p in body
-            )
-            if len(record["full_text"]) > 0:
-                records.append(record)
-                cfts_record["full_text"] = record["full_text"]
-                cfts_records.append(cfts_record)
+    # id
+    record["id"] = os.path.split(x)[-1].replace(".xml", "")
+    cfts_record["id"] = record["id"]
+    cfts_record["resolver"] = (
+        f"https://nuntiaturberichte.github.io/nbr-graz-static/{record['id']}.html"
+    )
+    record["rec_id"] = os.path.split(x)[-1].replace(".xml", "")
+    cfts_record["rec_id"] = record["rec_id"]
 
-make_index = client.collections[
-    "Nuntiatur Pius XI"
-].documents.import_(records)
+    # date
+    try:
+        year = doc.any_xpath(
+            ".//correspAction[@type='sent']/date[@when]"
+        )[0].attrib["when"]
+    except IndexError:
+        year = ""
+    if len(year) > 4:
+        record["year"] = int(year[:4])
+
+    # title
+    record["title"] = extract_fulltext(
+        doc.any_xpath(".//tei:titleStmt/tei:title")[0]
+    )
+    cfts_record["title"] = record["title"]
+
+    # sender
+    record["sender"] = []
+    cfts_record["persons"] = []
+    for y in doc.any_xpath(".//tei:correspAction[@type='sent']//tei:persName"):
+        record["sender"].append(normalize_string(y.text))
+        cfts_record["persons"].append(normalize_string(y.text))
+
+    # receiver
+    record["receiver"] = []
+    cfts_record["persons"] = []
+    for y in doc.any_xpath(".//tei:correspAction[@type='received']//tei:persName"):
+        record["receiver"].append(normalize_string(y.text))
+        cfts_record["persons"].append(normalize_string(y.text))
+
+    # text
+    record["full_text"] = extract_fulltext(body, tag_blacklist=tag_blacklist)
+    cfts_record["full_text"] = record["full_text"]
+    records.append(record)
+    cfts_records.append(cfts_record)
+
+# Dokumente in Typesense speichern
+make_index = client.collections[COLLECTION_NAME].documents.import_(records)
 print(make_index)
-print("done with indexing Nuntiatur Pius XI")
+print(f"✅ Fertig mit Indexierung {COLLECTION_NAME}")
 
 make_index = CFTS_COLLECTION.documents.import_(cfts_records, {"action": "upsert"})
 print(make_index)
-print("done with cfts-index Nuntiatur Pius XI")
+print(f"✅ Fertig mit CFTS-Indexierung {COLLECTION_NAME}")
